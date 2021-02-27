fb0c9068e6ba24b7605c9f3fcba400f2b58df14c
oscar abraham <oscara1706cl@gmail.com>
2021-02-27 16:17:02 -0600

    Añadimos añadir orden view, get roder view by id  y update order view to paid

--- ec_backend/base/signals.py
+++ ec_backend/base/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import pre_save
 from django.contrib.auth.models import User
+from .models import Product
 
 def updateUser(sender, instance, **kwargs):
     # print("Signal Triggered")
@@ -7,4 +8,14 @@ def updateUser(sender, instance, **kwargs):
     if user.email != '':
         user.username = user.email
 
+def checkStock(sender, instance, **kwargs):
+    # print("Signal Triggered")
+    product = instance
+    if product.countInStock <= 0:
+        product.out_of_stock = True
+        product.countInStock = 0
+    else:
+        product.out_of_stock = False
+
 pre_save.connect(updateUser,sender=User)
+pre_save.connect(checkStock,sender=Product)
